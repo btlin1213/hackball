77c008a089c87419d7df7f4fb3f16456c1823b0a
Andy Jiang <andypeiyuan@hotmail.com>
2020-08-20 23:22:35 +1000

    throw ball

--- server/room.js
+++ server/room.js
@@ -162,14 +162,25 @@ class Room {
             vx = (c2.x - c1.x) / dist,
             vy = (c2.y - c1.y) / dist;
 
-
-          console.log(players[index].flags & 2);
-          // Pick up it's a ball and currently haven't picked up any balls
-          if (
+            if (
+              // throw when space bar is pressed and there is a ball already picked up
+              players[i].body.type === BoardBody.TYPES.BALL  &&
+              players[index].flags & 2  &&
+              players[index].body.hasBall===players[i]
+              
+            ) {
+                // console.log("sdfadsfdhjsefbheabfjkfssa");
+                players[index].body.hasBall = null;
+                players[i].body.pickedUp = false;
+                vx *= 8;
+                vy *= 8;
+            }else if(
             players[i].body.type === BoardBody.TYPES.BALL && 
             (players[index].body.hasBall===players[i] || !players[index].body.hasBall)
           ) {
-              console.log("lol");
+              // pick up ball
+              
+              // console.log("lol");
               players[index].body.hasBall===players[i]
               players[index].body.hasBall = players[i];
               players[i].body.pickedUp = true;
@@ -182,18 +193,6 @@ class Room {
               //vy *= 8;
           } 
           
-          if (
-            // throw when space bar is pressed
-            players[i].body.type === BoardBody.TYPES.BALL  &&
-            players[index].flags & 2 &&
-            players[index].body.hasBall===players[i]
-          ) {
-              console.log("sdfadsfdsa");
-              players[index].body.hasBall = null;
-              players[i].body.pickedUp = false;
-              vx *= 8;
-              vy *= 8;
-          }
 
           // "weight"
           p1.v.mul(0.9);
